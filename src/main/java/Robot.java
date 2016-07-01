87165b95aba6c5fa4e54e7c069a4a79cf771c55d
Andrii_Vlasiuk <vlasiuk.andrii93@gmail.com>
2016-07-01 17:41:47 +0300

    sfsefse

--- src/main/java/Robot.java
+++ src/main/java/Robot.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class Robot implements Runnable {
     Robot( int number, int behaviour){
         threadNumber = number;
@@ -6,67 +8,87 @@ class Robot implements Runnable {
     }
 
         /*  main schema of robots' location
-    *       2(^3)cable2      R1      2(^0)fork1
-    *       R4                             R2
-    *       2(^2)fork2       R3      2(^1)cable1
+    *       2(^3)cable2      R3      2(^0)fork1
+    *       R4                             R1
+    *       2(^2)fork2       R2      2(^1)cable1
     */
 
     private Thread t;
     private int threadNumber;
     private int threadBehaviour;
     private int charge = 50;
+    String robotOwn = new String("empty_value");
+
+    public int time = 0;
+
 
 
     public void start () {
-        System.out.println("Start r" +  threadNumber );
+        System.out.println("r" + threadNumber + " started");
         if (t == null) {
             t = new Thread (this, String.valueOf(threadNumber));
             t.start ();
         }
+        Values.iterationArray[threadNumber-1]++;
+        do {} while (Values.iterationArray[0]!=Values.iterationArray[1] & Values.iterationArray[1]!=Values.iterationArray[2] & Values.iterationArray[2]!=Values.iterationArray[3] & Values.iterationArray[3]!=Values.iterationArray[0]);
+        System.out.println("r" + threadNumber + " iteratorArray=" + Arrays.toString(Values.iterationArray));
     }
 
     public void run() {
-        System.out.println("Running " +  threadNumber );
-        try {
-            for(int time = 0; time < 30; time+=0.5) {
-                System.out.println("Time:" + time);
-                printStatus();
-                doStartAction();
+        System.out.println("r" + threadNumber + " is running");
+        do{
+            //System.out.println("r = " + threadNumber + " Time:" + time + ". Start of iteration for this robot");
+            charge -= 5;
+
+            refreshOwning();
+            doStartAction();
+            printStatus();
+            refreshOwning();
+            doEndAction();
+            //System.out.println("r" + threadNumber + ". End of iteration for this robot");
+
+            Values.iterationArray[threadNumber-1]++;
+            do {} while (Values.iterationArray[0]!=Values.iterationArray[1] & Values.iterationArray[1]!=Values.iterationArray[2] & Values.iterationArray[2]!=Values.iterationArray[3] & Values.iterationArray[3]!=Values.iterationArray[0]);
+            System.out.println("r" + threadNumber + " iteratorArray=" + Arrays.toString(Values.iterationArray));
+
+            time += 0.5;
+
+            try {
                 Thread.sleep(3000);
-                doEndAction();
-                printStatus();
-                System.out.println("");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        } catch (InterruptedException e) {
-            System.out.println("Thread r" +  threadNumber + " interrupted.");
-        }
+        } while (charge != 0);
         System.out.println("Thread r" +  threadNumber + " exiting.");
     }
 
-
-    public void printStatus(){
-        String robotOwn = new String("empty_value");
+    public void refreshOwning(){
         switch (Values.own) {
             case "0000": robotOwn = "nothing";
-                         break;
+                break;
             case "0011": robotOwn = "fork1, cable1";
-                         break;
+                break;
             case "0110": robotOwn = "fork2, cable1";
-                         break;
+                break;
             case "1100": robotOwn = "fork2, cable2";
-                         break;
+                break;
             case "1001": robotOwn = "fork1, cable2";
-                         break;
+                break;
         }
-        System.out.println("r" + threadNumber + ": " + charge + "%" + ", has: " + robotOwn);
+    }
+
+    public void printStatus(){
+        refreshOwning();
+        System.out.println("r" + threadNumber + ": time=" + time + "  " + charge + "%" + ", has: " + robotOwn + " Behaviour=" + threadBehaviour);
     }
 
     public void doStartAction(){
         switch (threadBehaviour){
             case 1: //random
-                if (charge < 100 & Values.own == "0000"){
+                if (charge < 100){
                     Values.own = String.valueOf( Integer.toBinaryString((3 * threadNumber)) );
                     charge += 10;
+                    System.out.println("r" + threadNumber + " took stuff");
                 }
                 break;
             case 2: //gready
@@ -74,21 +96,26 @@ class Robot implements Runnable {
             case 3: //gentleman
                 break;
         }
-        System.out.println(threadNumber + " in progress...");
+
     }
 
     public void doEndAction(){
-        charge -= 5;
-        Values.chargeArray[ threadNumber ] = charge;
+        Values.chargeArray[ threadNumber - 1 ] = charge;
         // give or not to give stuff
         switch (threadBehaviour){
             case 1: //random
+                if (charge == 100 & Values.own != "0000"){
+                    int i = Integer.parseInt(Values.own);
+                    Values.own =  String.valueOf( (i/threadNumber) - 3 ); // make 0000 from 0011 (or 1001, or 1100, or 0110)
+                    System.out.println("r" + threadNumber + " gave away " + robotOwn);
+                }
                 break;
             case 2: //gready
                 break;
             case 3: //gentleman
                 break;
         }
+        //System.out.println("r" + threadNumber + " sleep -------------");
     }
 
 }
--- src/main/java/Values.java
+++ src/main/java/Values.java
@@ -2,5 +2,6 @@ public class Values {
 
     public static volatile String own = new String("0000");
     public static volatile int[] chargeArray = {-1,-1,-1,-1};
+    public static volatile int[] iterationArray = {0,0,0,0};
 
 }
